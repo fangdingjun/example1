33a2f5faea0db9fa915bf80648b461467251734e
fangdingjun <fangdingjun@gmail.com>
2014-04-29 17:06:46 +0800

    change atoi

--- c/my_atoi.c
+++ c/my_atoi.c
@@ -1,27 +1,24 @@
 #include <stdio.h>
-#include <math.h>
+//#include <math.h>
 #include <stdlib.h>
 
 int my_atoi(const char *p)
 {
-    int r1[50];
+    //int r1[50];
     int r2 = 0;
     char *p1;
-    int pos = 0;
-    int i;
-    p1 = (char *) p;
+    //int pos = 0;
+    //int i;
+
+    p1 = p;
     while (*p1 != '\0') {
-        // number is between 0x30 and 0x39
+        // number is between '0' and '9'
         // '0' = 0x30 '9' = 0x39
-        if (*p1 < 0x30 || *p1 > 0x39) {
+        if (*p1 < '0' || *p1 > '9') {
             break;
         }
-        r1[pos] = (*p1 - 0x30);
+        r2 = r2 * 10 + (*p1 - '0');
         p1++;
-        pos++;
-    }
-    for (i = 0; i < pos; i++) {
-        r2 += r1[i] * pow(10, pos - i - 1);
     }
     return r2;
 }
